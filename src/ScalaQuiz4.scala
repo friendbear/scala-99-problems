537ef450b97e4e6c4657c099141bf095cc6e13b4
gyagya <n.kusagaya@connectom.co.jp>
2017-03-03 07:59:34 +0900

    add 回答

---
+++ src/ScalaQuiz4.scala
@@ -0,0 +1,22 @@
+import scala.annotation.tailrec
+
+/**
+  * Created by n.kusagaya on 2017/03/02.
+  */
+object ScalaQuiz4 {
+  def main(args: Array[String]): Unit = {
+    val list = List(1, 1, 2, 3, 5, 8)
+    val listB = List("a", "b", "c", "d", "e", "f", "g")
+    println(length(listB))
+  }
+
+  def length[T](list: List[T]):Int = {
+    @tailrec
+    def loop(n:Int, tmp:List[T]):Int = tmp match {
+      case Nil  => n
+      case _::y => loop(n + 1, y)
+      case _    => sys.error("error!")
+    }
+    loop(0, list)
+  }
+}
