e6f5287a08a3df2a327ab3af9a41b826975480fa
gyagya <n.kusagaya@connectom.co.jp>
2017-03-02 09:03:26 +0900

    modify 回答を見て一部修正

--- src/ScalaQuiz4.scala
+++ src/ScalaQuiz4.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 /**
   * Created by n.kusagaya on 2017/03/02.
   */
@@ -9,10 +11,11 @@ object ScalaQuiz4 {
   }
 
   def length[T](list: List[T]):Int = {
+    @tailrec
     def loop(n:Int, tmp:List[T]):Int = tmp match {
-      case _::Nil => n
-      case _::y   => loop(n + 1, y)
-      case _      => sys.error("error!")
+      case Nil  => n
+      case _::y => loop(n + 1, y)
+      case _    => sys.error("error!")
     }
     loop(0, list)
   }
